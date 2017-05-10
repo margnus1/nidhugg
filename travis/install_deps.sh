f5f87bb6d0ab901600ef43b33fe6535997f8f55f
Magnus Lång <magnus.lang@it.uu.se>
2017-05-10 20:52:31 +0200

    .travis.yml: switch LLVM matrix to 3.[3456]

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -3,6 +3,12 @@ mkdir -p cache
 
 # LLVM
 case $LLVM_VERSION in
+    "3.3")
+        LLVM_URL="http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-amd64-Ubuntu-12.04.2.tar.gz"
+        ;;
+    "3.4")
+        LLVM_URL="http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-unknown-ubuntu12.04.tar.xz"
+	;;
     "3.9.1")
 	LLVM_URL="http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
 	;;
@@ -39,7 +45,13 @@ if [ ! -d $LLVM_DIR ] ; then
 	exit 1
     fi
     case $LLVM_VERSION in
-	"3.5.0")
+        "3.3")
+            mv cache/clang+llvm-$LLVM_VERSION-amd64-Ubuntu-12.04.2 cache/clang+llvm-$LLVM_VERSION
+	    ;;
+        "3.4")
+            mv cache/clang+llvm-$LLVM_VERSION-x86_64-unknown-ubuntu12.04 cache/clang+llvm-$LLVM_VERSION
+	    ;;
+        3.5.*)
 	    mv cache/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu cache/clang+llvm-$LLVM_VERSION
 	    ;;
 	"3.9.1")
