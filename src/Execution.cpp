a5af67dc45087c5a3ede4041633a49d8fbaeb94c
Magnus Lång <magnus.lang@it.uu.se>
2017-10-02 15:23:52 +0200

    Call register_alternatives in all alts
    Since they're recorded as symbolic events, we need them to appear in all the
    traces.

--- src/Execution.cpp
+++ src/Execution.cpp
@@ -3061,8 +3061,8 @@ void Interpreter::callAssume(Function *F, const std::vector<GenericValue> &ArgVa
 
 void Interpreter::callMalloc(Function *F,
                              const std::vector<GenericValue> &ArgVals){
+  if(conf.malloc_may_fail) TB.register_alternatives(2);
   if(conf.malloc_may_fail && CurrentAlt == 0){
-    TB.register_alternatives(2);
     GenericValue Result;
     Result.PointerVal = 0; // Return null
     returnValueToCaller(F->getReturnType(),Result);
