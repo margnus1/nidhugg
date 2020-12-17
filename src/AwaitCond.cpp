margnus1/nidhugg

commit fa468d0b3ad33d26cb1a62c43894f66aa5a61f3d

Add await conditions for signed values

src/AwaitCond.cpp
@@ -19,9 +19,32 @@
 
 #include "AwaitCond.h"
 #include <cstring>
+#include <climits>
+#include <llvm/Support/Host.h>
+
+namespace {
+  int smemcmp(const void *lhs, const void *rhs, std::size_t count) {
+    if (!count) return 0;
+    std::size_t big_byte_index;
+    if (llvm::sys::IsBigEndianHost) {
+      big_byte_index = count-1;
+    } else {
+      big_byte_index = 0;
+    }
+    bool lsign = ((const char*)lhs)[big_byte_index] >> (CHAR_BIT-1);
+    bool rsign = ((const char*)rhs)[big_byte_index] >> (CHAR_BIT-1);
+    if (lsign != rsign) return lsign ? -1 : 1;
+    return std::memcmp(lhs, rhs, count);
+  }
+}
 
 bool AwaitCond::satisfied_by(const void *data, std::size_t size) const {
-  int cmp = std::memcmp(data, operand.get(), size);
+  int cmp;
+  if ((cmp >> 3) & 0b1) {
+    cmp = smemcmp(data, operand.get(), size);
+  } else {
+    cmp = std::memcmp(data, operand.get(), size);
+  }
   unsigned shift;
   if (cmp < 0) {
     shift = 2;
@@ -35,7 +58,7 @@ bool AwaitCond::satisfied_by(const void *data, std::size_t size) const {
 
 const char *AwaitCond::name(Op op) {
   const static char *names[] = {
-    nullptr, "GT", "EQ", "GE", "LT", "NE", "LE",
+    nullptr, "UGT", "EQ", "UGE", "ULT", "NE", "ULE",
   };
   return names[op];
 }

src/AwaitCond.h
@@ -25,14 +25,25 @@
 #include "SymAddr.h"
 
 struct AwaitCond {
-  enum Op : int {
+  /* An Op is a bitfield, with the following meanings for bits 3..0, msb
+   * first:
+   *   Signed  - If the comparison is of signed values
+   *   Less    - If the comparison is true when lhs is less than rhs
+   *   Equal   - If the comparison is true when lhs is equal to rhs
+   *   Greater - If the comparison is true when lhs is greater than rhs
+   */
+  enum Op : unsigned {
     None,
-    GT = 0b001,
-    EQ = 0b010,
-    GE = 0b011,
-    LT = 0b100,
-    NE = 0b101,
-    LE = 0b110,
+    UGT = 0b0001,
+    EQ  = 0b0010,
+    UGE = 0b0011,
+    ULT = 0b0100,
+    NE  = 0b0101,
+    ULE = 0b0110,
+    SGT = 0b1001,
+    SGE = 0b1011,
+    SLT = 0b1100,
+    SLE = 0b1110,
   } op;
   SymData::block_type operand;
 

commit 9e015b65391d43a7d88e0f892e1054c99c21c308

Handle signed ops in AwaitCond::name()
Fixes: fa468d0b

src/AwaitCond.cpp
@@ -60,7 +60,10 @@ bool AwaitCond::satisfied_by(const void *data, std::size_t size) const {
 
 const char *AwaitCond::name(Op op) {
   const static char *names[] = {
-    nullptr, "UGT", "EQ", "UGE", "ULT", "NE", "ULE",
+    nullptr, "UGT",   "EQ",    "UGE",
+    "ULT",   "NE",    "ULE",   nullptr,
+    nullptr, "SGT",   nullptr, "SGE",
+    "SLT",   nullptr, "SLE",
   };
   return names[op];
 }