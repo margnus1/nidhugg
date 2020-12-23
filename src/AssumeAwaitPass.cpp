margnus1/nidhugg

commit 5137b7862509164b5e154cab5d8aea384ad83cef

await: LLVM 11 compatibility

src/AssumeAwaitPass.cpp
@@ -265,6 +265,9 @@ bool AssumeAwaitPass::tryRewriteAssume(llvm::Function *F, llvm::BasicBlock *BB, 
                    << ": Bad type " << Load->getType() << "\n";
       continue;
     }
+    assert(llvm::isa<llvm::FunctionType>(AwaitFunction->getType()));
+    llvm::FunctionType *AwaitFunctionType
+        = llvm::cast<llvm::FunctionType>(AwaitFunction->getType());
     llvm::dbgs() << "Wow, replacing " << *Load << " and " << *Call << " in " << F->getName() << "\n";
     llvm::IntegerType *i8Ty = llvm::Type::getInt8Ty(F->getParent()->getContext());
     llvm::ConstantInt *COp = llvm::ConstantInt::get(i8Ty, op);
@@ -272,7 +275,7 @@ bool AssumeAwaitPass::tryRewriteAssume(llvm::Function *F, llvm::BasicBlock *BB, 
     llvm::Value *Address = Load->getOperand(0);
     llvm::BasicBlock::iterator LI(Load);
     llvm::ReplaceInstWithInst(Load->getParent()->getInstList(), LI,
-                              llvm::CallInst::Create(AwaitFunction, {Address, COp, ArgVal, CMode}));
+                              llvm::CallInst::Create(AwaitFunctionType, AwaitFunction, {Address, COp, ArgVal, CMode}));
     return true;
   }
   if (blame_load)

src/AwaitCond.cpp
@@ -20,7 +20,9 @@
 #include "AwaitCond.h"
 #include <cstring>
 #include <climits>
-#include <llvm/Support/Host.h>
+/* One of these contains llvm::sys::IsBigEndianHost */
+#include <llvm/Support/Host.h>          /* On llvm < 11 */
+#include <llvm/Support/SwapByteOrder.h> /* On llvm >= 11 */
 
 namespace {
   int smemcmp(const void *lhs, const void *rhs, std::size_t count) {

src/Execution.cpp
@@ -3306,10 +3306,10 @@ bool Interpreter::isPthreadMutexLock(Instruction &I, GenericValue **ptr){
 
 bool Interpreter::isLoadAwait(Instruction &I, GenericValue **ptr, AwaitCond *cond){
   if(!isa<CallInst>(I)) return false;
-  CallSite CS(static_cast<CallInst*>(&I));
-  Function *F = CS.getCalledFunction();
+  AnyCallInst CI(static_cast<CallInst*>(&I));
+  Function *F = CI.getCalledFunction();
   if(!F || F->getName().str().rfind("__VERIFIER_load_await", 0) != 0) return false;
-  auto args = CS.arg_begin();
+  auto args = CI.arg_begin();
   *ptr = (GenericValue*)GVTOP(getOperandValue(args[0],ECStack()->back()));
   uint64_t op_int = getOperandValue(args[1],ECStack()->back()).IntVal.getZExtValue();
   AwaitCond::Op op(static_cast<AwaitCond::Op>(op_int));

commit 5a3a7a62f43d426c3ac2bde23badb4e7d49e12f7

AssumeAwaitPass: Fix function type argument bug
Fixes: 5137b7862509164b5e154cab5d8aea384ad83cef: await: LLVM 11 compatibility

src/AssumeAwaitPass.cpp
@@ -224,6 +224,17 @@ bool AssumeAwaitPass::runOnFunction(llvm::Function &F) {
   return changed;
 }
 
+static llvm::FunctionType *getFunctionType(llvm::Type *fptr) {
+  if (llvm::FunctionType *fty = llvm::dyn_cast<llvm::FunctionType>(fptr)) {
+    return fty;
+  } else if (llvm::PointerType *pty = llvm::dyn_cast<llvm::PointerType>(fptr)) {
+    return getFunctionType(pty->getElementType());
+  } else {
+    llvm::dbgs() << fptr << "\n";
+    ::abort();
+  }
+}
+
 bool AssumeAwaitPass::tryRewriteAssume(llvm::Function *F, llvm::BasicBlock *BB, llvm::Instruction *I) const {
   llvm::CallInst *Call = llvm::dyn_cast<llvm::CallInst>(I);
   if (!Call || !is_assume(Call)) return false;
@@ -265,9 +276,7 @@ bool AssumeAwaitPass::tryRewriteAssume(llvm::Function *F, llvm::BasicBlock *BB, 
                    << ": Bad type " << Load->getType() << "\n";
       continue;
     }
-    assert(llvm::isa<llvm::FunctionType>(AwaitFunction->getType()));
-    llvm::FunctionType *AwaitFunctionType
-        = llvm::cast<llvm::FunctionType>(AwaitFunction->getType());
+    llvm::FunctionType *AwaitFunctionType = getFunctionType(AwaitFunction->getType());
     llvm::dbgs() << "Wow, replacing " << *Load << " and " << *Call << " in " << F->getName() << "\n";
     llvm::IntegerType *i8Ty = llvm::Type::getInt8Ty(F->getParent()->getContext());
     llvm::ConstantInt *COp = llvm::ConstantInt::get(i8Ty, op);