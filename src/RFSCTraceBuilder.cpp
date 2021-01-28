margnus1/nidhugg

commit 980220bef88128a7c4352a3cb632a5e24d7022be

Add partially-working implementation of new rule

src/RFSCTraceBuilder.h
@@ -240,6 +240,7 @@ protected:
                                               index(index) {}
     AwaitCond cond;
     std::shared_ptr<DecisionNode> decision_ptr;
+    Option<int> planned_read_from;
     /* We don't use an IID as to not duplicate the IPid (used as key) */
     int index;
     int read_from = -1;
@@ -327,7 +328,7 @@ protected:
     /* The unfolding event corresponding to this executed event. */
     std::shared_ptr<RFSCUnfoldingTree::UnfoldingNode> event;
 
-    Option<int> read_from;
+    Option<int> read_from, planned_read_from;
     /* Symbolic representation of the globally visible operation of this event.
      * Empty iff !may_conflict
      */
@@ -486,18 +487,26 @@ protected:
    * should it be executed */
   bool check_happens_before_blocked(IID<IPid> event, IPid blocked,
                                     const SymAddr &blocked_on_addr) const;
-  /* Assigns unfolding events to all executed steps. Returns false if
-   * the current execution is found to be redundant with some other,
-   * otherwise true; */
-  bool compute_unfolding();
+  /* Assigns unfolding events to all executed steps, and calls plan(). */
+  void compute_unfolding_and_plan();
 
   /* Computes alternative traces based on the current trace by changing
    * single reads-from assignments, or swapping adjacent pairs of
    * locks/rmws. Requires unfolding&decision nodes and vector clocks. */
   void plan(VClock<IPid> horizon);
 
+  void debug_print(VClock<IPid> horizon) const;
+
   /* A vector clock that includes all events in this trace */
   VClock<IPid> top_clock() const;
+
+  /* Get the unfolding node of some events's po-predecessor (or null, if
+   * the first event) */
+  const std::shared_ptr<RFSCUnfoldingTree::UnfoldingNode> &
+  get_unf_parent(IID<IPid> iid) const;
+  /* Get the unfolding node of an event in prefix, or null, if -1. */
+  const std::shared_ptr<RFSCUnfoldingTree::UnfoldingNode> &
+  get_unf_read_from(int read_from) const;
 
   // TODO: Refactor RFSCUnfoldingTree and and deprecate these methods.
   // Workaround due to require access to parent while not having a root-node
@@ -591,7 +600,7 @@ public:
 
     /* We allow TraceEvent to not be copy-constructible by forbidding it
      * here too. */
-    TraceOverlay(const TraceOverlay&) = delete;
+    // TraceOverlay(const TraceOverlay&) = delete;
     TraceOverlay(TraceOverlay&&) = default;
     TraceOverlay &operator=(const TraceOverlay&) = delete;
     TraceOverlay &operator=(TraceOverlay&&) = default;
@@ -611,6 +620,8 @@ public:
     TraceOverlay(const RFSCTraceBuilder*, const writes_by_addr_ty&,
                  std::initializer_list<unsigned> preallocate = {});
     TraceOverlay(const RFSCTraceBuilder*, writes_by_addr_ty&&,
+                 std::initializer_list<unsigned> preallocate = {});
+    TraceOverlay(const TraceOverlay &,
                  std::initializer_list<unsigned> preallocate = {});
 
     std::size_t prefix_size() const;
@@ -633,7 +644,9 @@ public:
 
   private:
     boost::container::flat_map<unsigned,TraceEvent> prefix_overlay;
-    std::size_t _prefix_size;
+    /* ugly hack of the century */
+  public:std::size_t _prefix_size;
+  private:
     const RFSCTraceBuilder *tb;
   };
 

src/VClock.cpp
@@ -41,6 +41,8 @@ VClock<int>::VClock(const VClock &vc) : vec(vc.vec) {}
 
 VClock<int>::VClock(VClock &&vc) : vec(std::move(vc.vec)) {}
 
+VClock<int>::VClock(VClockRef ref) : vec(ref.base, ref.base + ref._size) {}
+
 VClock<int>::VClock(const std::initializer_list<int> &il) : vec(il) {
 #ifndef NDEBUG
   for(unsigned i = 0; i < vec.size(); ++i){
@@ -216,7 +218,7 @@ std::string VClock<int>::to_string() const{
   return ss.str();
 }
 
-VClockVec::Ref &VClockVec::Ref::operator-=(const Ref vc) {
+VClockRef &VClockRef::operator-=(const VClockRef vc) {
   assert(vc._size == _size);
   for (unsigned i = 0; i < _size; ++i) {
     if(vc.base[i] < base[i]){
@@ -226,14 +228,14 @@ VClockVec::Ref &VClockVec::Ref::operator-=(const Ref vc) {
   return *this;
 }
 
-VClockVec::Ref &VClockVec::Ref::operator=(const VClock<int> vc) {
+VClockRef &VClockRef::operator=(const VClock<int> vc) {
   for (unsigned i = 0; i < _size; ++i) {
     base[i] = vc[i];
   }
   return *this;
 }
 
-bool VClockVec::Ref::lt(const Ref vc) const{
+bool VClockRef::lt(const VClockRef vc) const{
   assert(_size == vc._size);
   bool less = false;
   for(unsigned i = 0; i < _size; ++i){

src/VClock.h
@@ -97,6 +97,29 @@ private:
   std::map<DOM,int> clocks;
 };
 
+class VClockRef final {
+  friend class VClockVec;
+  friend class VClock<int>;
+  VClockRef(int* base, unsigned size) : base(base), _size(size) {}
+  int* base;
+  unsigned _size;
+public:
+  VClockRef &operator=(const VClock<int> vc);
+  /* Assign this vector clock to (*this - vc). */
+  VClockRef &operator-=(const VClockRef vc);
+  unsigned size() const { return _size; }
+  int operator[](int d) const { assert(d >= 0 && unsigned(d) < _size); return base[d]; };
+  int &operator[](int d) { assert(d >= 0 && unsigned(d) < _size); return base[d]; };
+
+  /* *** Partial order comparisons ***
+   *
+   * A vector clock u is considered strictly less than a vector clock
+   * v iff for all d in DOM, it holds that u[d] <= v[d], and there is
+   * at least one d such that u[d] < v[d].
+   */
+  bool lt(const VClockRef vc) const;
+};
+
 template<>
 class VClock<int> final {
 public:
@@ -114,6 +137,7 @@ public:
   /* Create a vector clock which is a copy of vc. */
   VClock(const VClock<int> &vc);
   VClock(VClock<int> &&vc);
+  VClock(VClockRef ref);
   /* Create a vector clock that is a translation of vc, where the
    * clock of t[i] is initialized to vc[i]. The clocks of all d which
    * do not occur in t are initialized to 0.
@@ -188,27 +212,7 @@ public:
   VClockVec() : clock_size(0) {}
   VClockVec(unsigned clock_size, std::size_t size)
     : vec(clock_size*size), clock_size(clock_size) {}
-  class Ref final {
-    friend class VClockVec;
-    Ref(int* base, unsigned size) : base(base), _size(size) {}
-    int* base;
-    unsigned _size;
-  public:
-    Ref &operator=(const VClock<int> vc);
-    /* Assign this vector clock to (*this - vc). */
-    Ref &operator-=(const Ref vc);
-    unsigned size() const { return _size; }
-    int operator[](int d) const { assert(d >= 0 && unsigned(d) < _size); return base[d]; };
-    int &operator[](int d) { assert(d >= 0 && unsigned(d) < _size); return base[d]; };
-
-    /* *** Partial order comparisons ***
-     *
-     * A vector clock u is considered strictly less than a vector clock
-     * v iff for all d in DOM, it holds that u[d] <= v[d], and there is
-     * at least one d such that u[d] < v[d].
-     */
-    bool lt(const Ref vc) const;
-  };
+  typedef VClockRef Ref;
   Ref operator[](int d) {
     assert (d >= 0 && (std::size_t(d)+1)*clock_size <= vec.size());
     return { vec.data() + (d*clock_size), clock_size };

commit 0ef955832812dcb19ee81b5f92deb8740d01f24f

Fix small bugs in new implementation
Fixes: 980220be: Add partially-working implementation of new rule

src/RFSCTraceBuilder.cpp
@@ -1792,13 +1792,14 @@ void RFSCTraceBuilder::plan(VClock<IPid> horizon) {
         threads[jp].event_indices.push_back(j); // Not needed?
         maybe_add_spawn_happens_after(j);
         compute_above_clock(j);
+        horizon_overlay._prefix_size++;
 
         if (can_swap_by_vclocks(i, j)) {
           if (conf.debug_print_on_reset)
             llvm::dbgs() << "Trying to replace " << pretty_index(i)
                          << " with deadlocked " << pretty_index(j)
                          << ", after " << pretty_index(original_read_from);
-          TraceOverlay ol(this, writes_by_address, {unsigned(i), unsigned(j)});
+          TraceOverlay ol(horizon_overlay, {unsigned(i), unsigned(j)});
           ol.prefix_mut(j).read_from = original_read_from;
           ol.prefix_mut(i).decision_swap(ol.prefix_mut(j));
           assert(!ol.prefix_at(i).pinned());
@@ -2576,7 +2577,7 @@ TraceOverlay(const TraceOverlay &other,
              std::initializer_list<unsigned> preallocate)
   : writes_by_addr(other.writes_by_addr),
     blocking_awaits(other.blocking_awaits), _prefix_size(other._prefix_size),
-    tb(other.tb) {
+    tb(other.tb), prefix_overlay(other.prefix_overlay) {
   prefix_overlay.reserve(prefix_overlay.size() + preallocate.size());
   assert(std::is_sorted(preallocate.begin(), preallocate.end()));
   auto insertion_hint = prefix_overlay.end();

src/RFSCTraceBuilder.h
@@ -553,6 +553,8 @@ public:
     struct TraceEvent {
       TraceEvent(const IID<IPid> &iid, SymEv sym = {});
       TraceEvent(const Event &event);
+      TraceEvent(const TraceEvent &) = default;
+      TraceEvent &operator=(const TraceEvent&) = default;
       TraceEvent(TraceEvent &&) = default;
       TraceEvent &operator=(TraceEvent&&) = default;
       DecisionNode *decision_ptr;

tests/smoke/C-tests/dekker_lock_trylock.c
@@ -1,0 +1,1 @@
+../../await/dekker_lock_trylock.c