080ba11e9f3f577559f3e56c0ba407ea4ad779be
Magnus Lång <magnus.lang@it.uu.se>
2021-01-28 11:49:24 +0100

    Option: Add has_value() accessor

--- src/Option.h
+++ src/Option.h
@@ -27,51 +27,52 @@
 template<typename Value>
 class Option {
 public:
-  Option() : has_value(false) {}
-  Option(std::nullptr_t) : has_value(false) {}
-  Option(Value value) : has_value(true), value(std::move(value)) {}
-  ~Option() { if (has_value) value.~Value(); }
-  Option(const Option &o) : has_value(o.has_value) {
-    if (has_value) new((void*)&value) Value(o.value);
+  Option() : _has_value(false) {}
+  Option(std::nullptr_t) : _has_value(false) {}
+  Option(Value value) : _has_value(true), value(std::move(value)) {}
+  ~Option() { if (_has_value) value.~Value(); }
+  Option(const Option &o) : _has_value(o._has_value) {
+    if (_has_value) new((void*)&value) Value(o.value);
   }
-  Option(Option &&o) : has_value(o.has_value) {
-    if (has_value) new((void*)&value) Value(std::move(o.value));
+  Option(Option &&o) : _has_value(o._has_value) {
+    if (_has_value) new((void*)&value) Value(std::move(o.value));
   }
   Option &operator=(const Option &o) {
-    if (has_value) value.~Value();
-    if ((has_value = o.has_value)) new((void*)&value) Value(o.value);
+    if (_has_value) value.~Value();
+    if ((_has_value = o._has_value)) new((void*)&value) Value(o.value);
     return *this;
   }
   Option &operator=(Option &&o) {
-    if (has_value) value.~Value();
-    if ((has_value = o.has_value)) new((void*)&value) Value(std::move(o.value));
+    if (_has_value) value.~Value();
+    if ((_has_value = o._has_value)) new((void*)&value) Value(std::move(o.value));
     return *this;
   }
 
   void reset() {
-    if (std::exchange(has_value, false)) value.~Value();
+    if (std::exchange(_has_value, false)) value.~Value();
   }
 
-  operator bool() const noexcept { return has_value; }
-  Value const& operator *() const { assert(has_value); return value; }
-  Value& operator *() { assert(has_value); return value; }
-  Value const *operator ->() const { assert(has_value); return &value; }
-  Value *operator ->() { assert(has_value); return &value; }
+  operator bool() const noexcept { return _has_value; }
+  constexpr bool has_value() const { return _has_value; }
+  Value const& operator *() const { assert(_has_value); return value; }
+  Value& operator *() { assert(_has_value); return value; }
+  Value const *operator ->() const { assert(_has_value); return &value; }
+  Value *operator ->() { assert(_has_value); return &value; }
 
   Value const &value_or(const Value &def) const {
-    if (has_value) return value;
+    if (_has_value) return value;
     else return def;
   }
 
   /* Monadic bind; transform the value of the option (if any) */
   template<typename F> auto map(F f) const ->
     Option<typename std::result_of<F(const Value&)>::type> {
-    if (!has_value) return {};
+    if (!_has_value) return {};
     else return f(value);
   }
 
 private:
-  bool has_value;
+  bool _has_value;
   union{
     Value value;
   };
