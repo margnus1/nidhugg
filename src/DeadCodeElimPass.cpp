4f35ca7f09711982b0ca617655f07eacbbc4bb71
Magnus Lång <magnus.lang@it.uu.se>
2021-03-23 12:54:55 +0100

    DeadCodeElimPass: LLVM 3.8 compat, cleanup
    llvm::Instruction::isSafeToRemove() is not in llvm 3.8, and is implied
    by the other predicates anyway.

--- src/DeadCodeElimPass.cpp
+++ src/DeadCodeElimPass.cpp
@@ -32,25 +32,15 @@ void DeadCodeElimPass::getAnalysisUsage(llvm::AnalysisUsage &AU) const{
 
 namespace {
   typedef llvm::SmallPtrSet<llvm::Value*, 4> UseSet;
-  typedef llvm::ValueMap<llvm::BasicBlock*, UseSet> UseSets;
 
   bool unsafeToDelete(const llvm::Instruction &I) {
     return I.mayReadOrWriteMemory()
-      || !I.isSafeToRemove()
       || !llvm::isSafeToSpeculativelyExecute(&I)
       || I.isTerminator();
   }
 
-  UseSet out(UseSets &in, llvm::BasicBlock &BB) {
+  UseSet computeUseSets(llvm::Function &F) {
     UseSet use;
-    for (auto it = pred_begin(&BB); it != pred_end(&BB); ++it) {
-      auto &out1 = in[*it];
-      use.insert(out1.begin(), out1.end());
-    }
-    return use;
-  }
-
-  void computeUseSets(UseSet &use, llvm::Function &F) {
     size_t old_size;
     do {
       old_size = use.size();
@@ -66,20 +56,19 @@ namespace {
         }
       }
     } while(use.size() != old_size);
+    return use;
   }
 }
 
 bool DeadCodeElimPass::runOnFunction(llvm::Function &F) {
   size_t deleted = 0;
-  UseSet use;
-  computeUseSets(use, F);
+  UseSet use = computeUseSets(F);
 
   auto &BBL = F.getBasicBlockList();
   for (auto BBI = BBL.rbegin(); BBI != BBL.rend(); ++BBI) {
     for (auto it = BBI->end(); it != BBI->begin();) {
       llvm::Instruction &I = *--it;
-      if (unsafeToDelete(I) || use.count(&I)) {
-      } else {
+      if (!(unsafeToDelete(I) || use.count(&I))) {
         it = I.eraseFromParent();
         ++deleted;
       }
