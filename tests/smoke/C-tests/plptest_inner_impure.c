8a51e4fcbdbeb50dfb7b4d2c349356dadbb37a4a
Magnus Lång <margnus1@telia.com>
2022-06-02 17:11:58 +0200

    Make impure inner loops testcase more robust
    Fixes: 2b09b6e2: Add testcase demonstrating impure inner loops

--- tests/smoke/C-tests/plptest_inner_impure.c
+++ tests/smoke/C-tests/plptest_inner_impure.c
@@ -4,20 +4,32 @@
 #include <stdbool.h>
 #include <assert.h>
 
+void __VERIFIER_assume(bool b);
+
 atomic_int x;
 atomic_int y;
+atomic_int z;
+atomic_int w = 0;
+atomic_int *p;
 
 static void *test(void *arg) {
   while(true) {
-    bool did_y = false;
-    int a;
-    while(true) {
-      a = x;
-      if (a) break;
-      if (!did_y) { y++; did_y = true; }
-      if (did_y) break;
+    int a = x;
+    bool did_z = false;
+    int b;
+    /* Can only incement z once per iteration of the outer loop, and
+     * only exit once b != 0
+     */
+    while(!(b = y)) {
+      if (!did_z) { z++; did_z = true; }
     }
     if (a) break;
+    (void)*p;
+    /* PLP would insert __VERIFIER_assume(!b) here, trying to say that
+     * the outer loop is pure if the inner loop is not taken.
+     * However, this actually filters out executions where the inner
+     * loop is taken; thus making the assertion in main unsatisfiable.
+     */
   }
 
   return arg;
@@ -25,7 +37,11 @@ static void *test(void *arg) {
 
 int main() {
   pthread_t t;
+  p = &w;
   pthread_create(&t, NULL, test, NULL);
+  y = 1;
+  y = 0;
+  y = 1;
   x = 1;
-  assert(y < 2);
+  assert(z < 2);
 }
