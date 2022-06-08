d979c5bdf58e51e6b224878f1e92e05ccabd1639
Magnus Lång <margnus1@telia.com>
2022-06-08 13:02:08 +0200

    PLP: Fix bug with true pc with insertion location

--- src/PartialLoopPurityPass.cpp
+++ src/PartialLoopPurityPass.cpp
@@ -1303,7 +1303,7 @@ namespace {
       }
       //llvm::dbgs() << " Insertion point: " << *I << "\n";
       llvm::Value *Cond = nullptr;
-      if (conj.is_true()) {
+      if (!conj.has_conjuncts()) {
         Cond = llvm::ConstantInt::getTrue(L->getHeader()->getContext());
       } else {
         for (const BinaryPredicate &term : conj) {
